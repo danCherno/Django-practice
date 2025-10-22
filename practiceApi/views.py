fd41bf00310c50f47c9ab87a082c428da54b8e4f
Cherno <daniel.cherno9@gmail.com>
2025-10-22 19:28:41 +0300

    added login API

--- practiceApi/views.py
+++ practiceApi/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import status, viewsets, filters
 
 from practiceApi import serializers, models, permissions
@@ -96,3 +98,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ('email', )
+
+class UserLoginApiView(ObtainAuthToken):
+    """ Handle creating user authentication tokens """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
