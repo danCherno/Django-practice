c2479e63b160f75d3003f2e258ea422b6107a51a
Cherno <daniel.cherno9@gmail.com>
2025-10-22 12:17:12 +0300

    added HelloApiView

--- practiceApi/views.py
+++ practiceApi/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """ Test API View """
+    def get(self, request, format=None):
+        an_apiview = [
+          'this is',
+          'a test',
+          'to see if',
+          'i can work with',
+          'lists'  
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
