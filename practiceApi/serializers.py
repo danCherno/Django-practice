3fa0d82bf8ab7304271fb82ca3008fc9c0af8ca7
Cherno <daniel.cherno9@gmail.com>
2025-10-22 18:55:55 +0300

    added profiles viewset

--- practiceApi/serializers.py
+++ practiceApi/serializers.py
@@ -1,5 +1,35 @@
 from rest_framework import serializers
 
+from practiceApi import models
+
 class HelloSerializer(serializers.Serializer):
     """ Serializes a name field for testing our APIView """
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """ Serializes a user profile object """
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_User(
+            email = validated_data['email'],
+            password = validated_data['password'],
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+        
+        return super.update(instance, validated_data)
--- practiceApi/views.py
+++ practiceApi/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import status, viewsets
 
-from practiceApi import serializers
+from practiceApi import serializers, models, permissions
 
 # Create your views here.
 
@@ -85,3 +86,11 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk=None):
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handle creating and updating profiles """
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
