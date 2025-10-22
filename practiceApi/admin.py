a566b3bf67e3c341c7fea6ea9f668029cae8b073
Cherno <daniel.cherno9@gmail.com>
2025-10-22 11:21:30 +0300

    enabled django admin for user profiles

--- practiceApi/admin.py
+++ practiceApi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from practiceApi import models
+
 # Register your models here.
+
+admin.site.register(models.UserProfile)
--- practiceApi/models.py
+++ practiceApi/models.py
@@ -9,46 +9,38 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """manager for user profiles"""
 
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, password=None):
         if not email:
             raise valueError('User must have an email adress')
         
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-        def create_superuser(self, email, name, pasword):
-            user = self.create_user(email, name, password)
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
 
-            user.is_superuser = True
-            user.is_staff = True
+        user.is_superuser = True
+        user.is_staff = True
 
-            user.save(using=self._db)
+        user.save(using=self._db)
 
-            return user
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
-
-    def get_full_name(self):
-        return self.name
-
-    def get_short_name(self):
-        return self.name
 
     def __str__(self):
         return self.email
